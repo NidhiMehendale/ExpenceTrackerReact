9040166398566cc3143c70646db012595fed90cb
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-04-21 15:51:55 +0530

    Starting with react coding

---
+++ src/components/Expenceitem.js
@@ -0,0 +1,13 @@
+function ExpenceItem() {
+    return (
+        <div>
+            <h2>Expence Items</h2>
+            <div> Food Rs10 </div>
+            <div> Petrol Rs100 </div>
+            <div> Movies Rs200 </div>
+       </div>
+        
+    )
+}
+
+export default ExpenceItem;
