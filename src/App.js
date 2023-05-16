8e2c54d9751155dde393a394458249a22365375f
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-05-16 17:15:10 +0530

    Making app dynamic

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React from "react";
-import ExpenseForm from "./components/Expenses/ExpenseForm";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/Expenses/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Purse',
+    amount: 94.12,
+    date: new Date(2023, 2, 12),
+    LocationOfExpenditure : 'Dombivli',
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 1,10),LocationOfExpenditure : 'Kalyan', },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2023, 2, 28),
+    LocationOfExpenditure : 'Thane',
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2023, 3, 12),
+    LocationOfExpenditure : 'Mumbai',
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Purse',
-      amount: 94.12,
-      date: new Date(2023, 2, 12),
-      LocationOfExpenditure : 'Dombivli',
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 1,10),LocationOfExpenditure : 'Kalyan', },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2023, 2, 28),
-      LocationOfExpenditure : 'Thane',
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2023, 3, 12),
-      LocationOfExpenditure : 'Mumbai',
-    },
-  ];
-  
+   
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = expense => {
-    console.log(expense);
+    setExpenses( (prevExpenses) => {
+        return [expense, ...prevExpenses];
+    }); 
   };
 
   return (
